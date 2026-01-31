ea10a6fc7856e291777ac7fc3c4b724ef77bb978
nxtkofi <psliwa.b@gmail.com>
2026-01-31 22:30:43 +0100

    refactor: move everything 1 dir up

---
+++ src/CatMemo/src/components/CatImageUwu.tsx
@@ -0,0 +1,10 @@
+import CatPng from "../../public/favicon.png";
+
+function CatImage() {
+  return (<div className="fixed -bottom-1 left-4 bg-blue-1">
+    <img className="w-20 md:w-32 lg:w-40" src={CatPng} alt="Meow!!!" />
+  </div>)
+  #abebeb
+}
+
+export default CatImage;
--- src/components/CatImage.tsx
+++
@@ -1,9 +0,0 @@
-import CatPng from "../../public/favicon.png";
-
-function CatImage() {
-    return ( <div className="fixed -bottom-1 left-4">
-        <img className="w-20 md:w-32 lg:w-40" src={CatPng} alt="Meow!!!" />
-    </div> );
-}
-
-export default CatImage;
